ec22ce3f2619302b545929535f281d9800f9b984
Gurleenkaurmakhija <130361214+Gurleenkaurmakhija@users.noreply.github.com>
2025-06-19 10:50:17 +0530

    Time: 488 ms (56.92%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,9 +2,10 @@
 /* if there would have been entry 5 2025-02-06 then previous row with id 4 was ot previous DAY it was row, we need to conisder previous row
 -->here inner and self join is same 
 -->datediff*/
-select w1.id 
+select w1.id
 from Weather w1
 INNER JOIN weather w2
+/*date difference as of 1*/
 
 on datediff(w1.recordDate,w2.recordDate)=1
 AND w1.temperature>w2.temperature;
