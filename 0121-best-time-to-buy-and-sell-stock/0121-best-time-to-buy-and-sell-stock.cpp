0befd518d95a98fe87652d454f220a8d867f49f1
Gurleenkaurmakhija <130361214+Gurleenkaurmakhija@users.noreply.github.com>
2025-07-03 13:37:18 +0530

    Time: 0 ms (100%), Space: 97.5 MB (25.62%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,12 +4,12 @@ public:
         int cost=INT_MAX;
         int sell=0;
        
-        for(int i=0;i<prices.size();i++){
+        for(int i=0;i<prices.size()-1;i++){
             if(prices[i]<cost){
                 cost=prices[i];
             }
-            if(prices[i]>cost){
-                sell=max(sell,prices[i]-cost);
+            if(prices[i+1]>cost){
+                sell=max(sell,prices[i+1]-cost);
             }
 
         }
